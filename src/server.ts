3f425b81929b6392c799a8dee409d57ebe2b4593
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-03-13 03:19:41 +0900

    fix: connected  mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
+
+
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGO_URL as string, {})
+.then((data) => {
+console.log("MongoDB connection succeed");
+const PORT = process.env.PORT ?? 3005
+//app.listen(``)
+})
+.catch(err =>{
+console.log("ERRORon connection MongoDB", err);
+})
